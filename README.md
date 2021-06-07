439f129968ba27bbbe509a49f936bf2baf175147
Dmitrii N <dmitrii@dev>
2021-06-07 16:06:50 +0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# yoshows
- A firefox addon for myshows.me adding link yohoho.cc by altering html elements.
+# YoShows
+
+Расширение для сайта <a href="https://myshows.me">myshows.me</a>, которое добавляет ссылку на <a href="https://yohoho.cc">yohoho.cc</a> для онлайн-просмотра любимого сериала.
+
+### Установка с магазина расширений
+
+<b>FireFox: </b> <a href="https://addons.mozilla.org/ru/firefox/addon/yoshows/">YoShows</a>
+
+<b>Chrome: </b> <i>скоро...</i>
--- main.js
+++ main.js
@@ -31,22 +31,29 @@ class Yo {
         }
     }
 
+    static render() {
+        const path = window.location.pathname
+        if (path === "/profile/") {
+            Yo.renderProfile().catch(error => console.log(error.message))
+        } else if (path === "/search/all/") {
+            Yo.renderSearch().catch(error => console.log(error.message))
+        } else if (/view\/\d+\/+$/ig.exec(path)) {
+            Yo.renderView(path.match(/\d+/g).shift()).catch(error => console.log(error.message))
+        }
+    }
+
     static watch() {
         const observer = new MutationObserver((mutations, observer) => {
             for (let mutation of mutations) {
                 Array.from(mutation.removedNodes, node => {
                     if (node.classList.contains("nuxt-progress")) {
-                        const path = window.location.pathname
-                        if (path === "/profile/") {
-                            Yo.renderProfile().catch(error => console.log(error))
-                        } else if (/view\/\d+\/+$/ig.exec(path)) {
-                            Yo.renderView(path.match(/\d+/g).shift()).catch(error => console.log(error))
-                        }
+                        this.render()
                     }
                 })
             }
         });
         observer.observe(document.getElementById(this.rootId), this.observerConfig);
+        this.render()
     }
 
     static fetchQuery(showId) {
@@ -58,23 +65,52 @@ class Yo {
     static renderProfile() {
         return Promise.all(Array.from(document.getElementsByClassName("Unwatched-showTitle"), item => {
             const showId = item.href.match(/\d+/g).shift()
+            const element = Array.from(item.closest("div.Row-container").getElementsByClassName("Unwatched-remain")).pop()
             return this.fetchQuery(showId).then(result => {
                 return {
                     id: result,
-                    element: item.closest("div.Row-container")
+                    element: element,
                 }
             })
-        })).then(r => console.log(r))
+        })).then(result => result.map(value => this.createYoNode(value)))
+    }
+
+    static renderSearch() {
+        return Promise.all(Array.from(document.getElementsByClassName("ShowCol-title"), item => {
+            const showId = item.firstElementChild.href.match(/\d+/g).shift()
+            const element = Array.from(item.parentElement.getElementsByClassName("ShowCol-titleOriginal")).pop()
+            return this.fetchQuery(showId).then(result => {
+                return {
+                    id: result,
+                    element: element,
+                }
+            })
+        })).then(result => result.map(value => this.createYoNode(value)))
     }
 
     static renderView(showId) {
         return this.fetchQuery(showId).then(result => {
-            const element = Array.from(document.getElementsByClassName("ShowStatusBar-list")).shift()
+            const element = Array.from(document.getElementsByClassName("ShowStatusBar-option")).pop()
             return {
                 id: result,
-                element: element.lastChild
+                element: element
             }
-        }).then(r => console.log(r))
+        }).then(r => this.createYoNode(r))
+    }
+
+    static createYoNode(data) {
+        if (data.element.dataset.id === data.id.toString()) {
+            return
+        }
+        const a = document.createElement('a')
+        a.setAttribute('data-id', data.id)
+        a.setAttribute('target', '_blank')
+        a.setAttribute('href', this.yoURL + data.id)
+        a.classList.add(data.element.classList.item(0))
+        const span = document.createElement('span')
+        span.innerText = 'Смотреть на Yohoho'
+        a.append(span)
+        return data.element.after(a)
     }
 }
 
